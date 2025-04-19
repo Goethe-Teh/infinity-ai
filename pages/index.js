6dfc56f458ff60aedaa9949e54a88d60d6141ec7
Goethe-Teh <bdwchatgpt@gmail.com>
2025-04-20 02:49:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,26 +31,23 @@ export default function Home() {
   ];
 
   return (
-    <div style={{ padding: '30px' }}>
+    <div style={{ padding: 30 }}>
       <h2>Infinity AI</h2>
-
       <p>
         <b>สวัสดีค่ะ ดิฉัน กำลังจะกลายเป็นคนพิเศษส่วนตัวของคุณ</b><br />
         โปรดตั้งชื่อ กำหนดเพศ อายุ บุคลิก รูปร่าง หน้าตา ลักษณะนิสัย ความสามารถ หรือคุณสมบัติพิเศษ<br />
         และสถานะความสัมพันธ์ระหว่างเรา เช่น เพื่อนสนิท เพื่อนร่วมงาน แฟน ฯลฯ ตามที่คุณปรารถนา<br />
-        แล้วพบกันในไม่กี่วินาทีข้างหน้านะคะ
+        แล้วพบกันในอีกไม่กี่วินาทีข้างหน้านะคะ
       </p>
-
-      <h4>กรุณาเลือกภาษา / Please select your LANGUAGE:</h4>
+      <h4>Please select your LANGUAGE:</h4>
       <select value={language} onChange={(e) => setLanguage(e.target.value)}>
         <option value="">-- Select Language --</option>
         {languages.map((lang) => (
           <option key={lang.code} value={lang.code}>{lang.label}</option>
         ))}
       </select>
-
       <br /><br />
-      <button onClick={handleStart}>เริ่มต้น / Start</button>
+      <button onClick={handleStart}>Start</button>
     </div>
   );
 }
